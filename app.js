e8c28f185ded9698bf53d31f02468040d1e7113f
Vaibhav Singh <vaibhavsingh31390@gmail.com>
2023-12-07 23:01:15 +0530

    addecors

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const app = express();
+const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Views/EmailTemplates"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
 
+app.use(cors(corsOptions));
 const emailRoutes = require("./Routes/emailRoutes");
 
 app.use("/api/v1", emailRoutes);
